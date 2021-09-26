2293cf18219656fb56b42ae79d23c9cc35467e0c
Vanshaj Sharma <44530472+vanshaj8@users.noreply.github.com>
2021-09-27 02:53:17 +0530

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,73 @@
+import { Button } from "@chakra-ui/button";
+import { useColorMode } from "@chakra-ui/color-mode";
+import { Image } from "@chakra-ui/image";
+import { Stack, Circle, Flex, Box, Text } from "@chakra-ui/layout";
+import { useMediaQuery } from "@chakra-ui/media-query";
+import React from "react";
+
+function Header() {
+  const { colorMode } = useColorMode();
+  const isDark = colorMode === "dark";
+
+  const [isNotSmallerScreen] = useMediaQuery("(min-width:600px)");
+
+  return (
+    <Stack>
+      <Circle
+        position="absolute"
+        bg="blue.100"
+        opacity="0.1"
+        w="300px"
+        h="300px"
+        alignSelf="flex-end"
+      />
+      <Flex
+        direction={isNotSmallerScreen ? "row" : "column"}
+        spacing="200px"
+        p={isNotSmallerScreen ? "32" : "0"}
+        alignSelf="flex-start"
+      >
+        <Box mt={isNotSmallerScreen ? "0" : 16} align="flex-start">
+          <Text fontSize="5xl" fontWeight="semibold">
+            Hi, I am
+          </Text>
+          <Text
+            fontSize="7xl"
+            fontWeight="bold"
+            bgGradient="linear(to-r, cyan.400, blue.500, purple.600)"
+            bgClip="text"
+          >
+            Vanshaj Sharma
+          </Text>
+          <Text color={isDark ? "gray.200" : "gray.500"}>
+            4th Year Computer Science Student From Chandigarh University , React
+            Js Developer at HighRadius Corporation.
+          </Text>
+          <Button
+            mt={8}
+            colorScheme="blue"
+            onClick={() =>
+              window.open(
+                "https://www.linkedin.com/in/vanshaj-sharma-9239481a1/"
+              )
+            }
+          >
+            Hire Me
+          </Button>
+        </Box>
+        <Image
+          alignSelf="center"
+          mt={isNotSmallerScreen ? "0" : "12"}
+          mb={isNotSmallerScreen ? "0" : "12"}
+          borderRadius="full"
+          backgroundColor="transparent"
+          boxShadow="lg"
+          boxSize="300px"
+          src="https://avatars.githubusercontent.com/u/44530472?v=4"
+        />
+      </Flex>
+    </Stack>
+  );
+}
+
+export default Header;
